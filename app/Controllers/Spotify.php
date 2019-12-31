6f09f7e7176481d3232d46ef5d20ad055a067aad
Povilas Brilius <pbrilius@gmail.com>
2019-12-31 09:41:43 +0200

    Cloud storage required

--- app/Controllers/Spotify.php
+++ app/Controllers/Spotify.php
@@ -25,14 +25,12 @@ class Spotify extends BaseController
 		$state = hash('haval160,4', uniqid('sp'));
 		$session->set('state', $state);
 
-		var_dump(getenv('CLIENT_ID'));
-
 		$data = [
 			'state'       => $state,
 			'redirectUrl' => site_url('/spotify-access'),
 		];
 		$session->destroy();
 
-		return view('spotify_implicit_grant', $data, ['cache' => 360]);
+		return view('spotify_implicit_grant', $data, ['cache' => 2]);
 	}
 }
--- app/Views/spotify_implicit_grant.php
+++ app/Views/spotify_implicit_grant.php
@@ -2,7 +2,7 @@
 <html>
     <script type="text/javascript">
         window.location='https://accounts.spotify.com/authorize?'
-        + 'client_id=<?= getenv('CLIENT_ID'); ?>'
+        + 'client_id=<?= getenv('CLIENT_ID'); ?>&'
         + 'response_type=token&'
         + 'redirect_uri=<?= urlencode($redirectUrl) ?>&'
         + 'state=<?= $state ?>';
