c1b693c47fde200633da48bd919e97a209f4aa24
Povilas Brilius <pbrilius@gmail.com>
2020-01-02 13:48:26 +0200

    Dev stie launching

--- app/Commands/GrantCommand.php
+++ app/Commands/GrantCommand.php
@@ -3,6 +3,7 @@
 namespace A6\Commands;
 
 use CodeIgniter\CLI\BaseCommand;
+use CodeIgniter\CLI\CLI;
 
 class GrantCommand extends BaseCommand
 {
@@ -12,5 +13,9 @@ class GrantCommand extends BaseCommand
 
 	public function run(array $params)
 	{
+		CLI::write('Starting to request access');
+		CLI::write('Client ID: ' . CLI::color(getenv('CLIENT_ID'), 'yellow'));
+
+		$this->logger->log('info', '[INFO] Starting to grant access to API - client ID {clientId}', ['clientId' => getenv('CLIENT_ID')]);
 	}
 }
