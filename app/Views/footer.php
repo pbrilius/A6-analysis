6da210e70588de14b67bd6fb8e5445bdeb012f2e
Povilas Brilius <pbrilius@gmail.com>
2020-01-03 15:32:32 +0200

    Footer credits

--- app/Views/footer.php
+++ app/Views/footer.php
@@ -1,5 +1,5 @@
 <div class="card" style="width: 18rem;">
-  <img class="card-img-top" src="<?= getenv('blog.logo') ?>" alt="Card image cap" height="180" width="286">
+  <img class="card-img-top" src="<?= getenv('blog.logo') ?>" alt="PB Group - developer - portfolio" height="180" width="286">
   <div class="card-body">
     <h5 class="card-title">Porfolio</h5>
     <p class="card-text">pbgroup.com blog and projects digests - SME eCommerce, eFinance & eCredits development.</p>
