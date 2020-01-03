fe0290c1bbc4775458c09b1fbbda5bd8ef931e81
Povilas Brilius <pbrilius@gmail.com>
2020-01-03 20:49:50 +0200

    Window tab opping link

--- app/Views/footer.php
+++ app/Views/footer.php
@@ -4,7 +4,7 @@
 		<div class="card-body">
 			<h5 class="card-title">Porfolio</h5>
 			<p class="card-text">pbgroup.com blog and projects digests - SME eCommerce, eFinance & eCredits development.</p>
-			<a href="<?= getenv('blog.url') ?>" class="btn btn-primary">PB Group</a>
+			<a href="<?= getenv('blog.url') ?>" class="btn btn-primary" target="_blank">PB Group</a>
 		</div>
 	</div>
 </div>
