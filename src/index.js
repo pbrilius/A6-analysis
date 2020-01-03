77ade3417400162f7eb91e44fb4941fedc13fa40
Povilas Brilius <pbrilius@gmail.com>
2020-01-03 20:36:14 +0200

    Themed London orange - warning/notice jacket preset

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import _ from 'lodash';
 import 'bootstrap';
 
-@import "custom";
-@import "~bootstrap/scss/bootstrap";
-
 function component() {
     const element = document.createElement('div');
   
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: ['./src/index.js', './src/_custom.scss'],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'public/dist'),
@@ -11,25 +11,27 @@ module.exports = {
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(scss)$/,
+        use: [{
+          loader: 'style-loader', // inject CSS to page
+        }, {
+          loader: 'css-loader', // translates CSS into CommonJS modules
+        }, {
+          loader: 'postcss-loader', // Run post css actions
+          options: {
+            plugins: function () { // post css plugins, can be exported to postcss.config.js
+              return [
+                require('precss'),
+                require('autoprefixer')
+              ];
+            }
+          }
+        }, {
+          loader: 'sass-loader' // compiles Sass to CSS
+        }]
       }
     ]
   },
-  test: /\.(scss)$/,
-    use: [{
-      loader: 'style-loader', // inject CSS to page
-    }, {
-      loader: 'css-loader', // translates CSS into CommonJS modules
-    }, {
-      loader: 'postcss-loader', // Run post css actions
-      options: {
-        plugins: function () { // post css plugins, can be exported to postcss.config.js
-          return [
-            require('precss'),
-            require('autoprefixer')
-          ];
-        }
-      }
-    }, {
-      loader: 'sass-loader' // compiles Sass to CSS
-    }]
 };
