ef65ec0e8ccde5cb804f465fd975fa4d9ec71b19
Povilas Brilius <pbrilius@gmail.com>
2020-01-03 13:21:39 +0200

    Default layouting

---
+++ app/Views/default.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+<head>
+	<?= $this->renderSection('bootstrap') ?>
+    <title><?= $title ?></title>
+</head>
+<body>
+    <?= $this->renderSection('content') ?>
+</body>
+<?= $this->renderSection('scripts') ?>
+</html>
